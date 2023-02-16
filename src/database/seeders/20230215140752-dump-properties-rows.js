d54a9fdf9dc591d794fadbb14997b9990dd64c58
Gabriel <gabrielsa95.2@gmail.com>
2023-02-16 10:49:43 -0300

    mrigrating properties table and dumping seed

---
+++ src/database/seeders/20230215140752-dump-properties-rows.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const propertiesData = require('../../../api-imobzi/properties')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('properties', await propertiesData, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('properties', null, {});
+  }
+};

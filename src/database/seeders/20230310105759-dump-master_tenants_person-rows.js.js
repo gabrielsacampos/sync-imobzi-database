c50500d12e26e36190044cdcf272910626066df7
Gabriel <gabrielsa95.2@gmail.com>
2023-03-10 08:28:31 -0300

    dumping tenants at leases table

---
+++ src/database/seeders/20230310105759-dump-master_tenants_person-rows.js.js
@@ -0,0 +1,27 @@
+'use strict';
+const tenantsPromise = require("../../../api-imobzi/tenants").getMasterTenants()
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) { 
+    const tenantsResolved = await tenantsPromise;
+    for(const index in  tenantsResolved){
+      await queryInterface.bulkUpdate(
+        'leases', 
+        {"master_tenant_person": tenantsResolved[index].tenant}, 
+        { id: tenantsResolved[index].lease}
+      );
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    const tenantsResolved = await tenantsPromise;
+    for(const index in  tenantsResolved){ 
+      await queryInterface.bulkDelete(
+        'leases', 
+        {"master_tenant_person": null}, 
+        { id: tenantsResolved[index].lease}
+      )
+      };
+  }
+};
